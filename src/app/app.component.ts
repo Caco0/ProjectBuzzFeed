0430f3f02c2b6b8704d5aede61ddc18ef4818f92
Caco0 <cacotpds@gmail.com>
2024-02-07 12:00:14 -0300

    Re-adding the project from scratch to fix issues in the SSH key. Finished project

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, HomeComponent],
+})
+export class AppComponent {
+  title = 'project-buzzfeed';
+}
